4c70747932237bada0f045495a06b226d16ce332
eliyajoseph7 <exaveryeliya20@gmail.com>
2024-01-16 14:25:13 +0300

    modified

--- frontend/myblog/src/app/app.module.ts
+++ frontend/myblog/src/app/app.module.ts
@@ -3,24 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './public/home/index/index.component';
-import { NavbarComponent } from './public/home/navbar/navbar.component';
 import { AnnouncementsComponent } from './public/home/contents/announcements/announcements.component';
 import { PostsComponent } from './public/home/contents/posts/posts.component';
 import { RelatedComponent } from './public/home/contents/related/related.component';
 import { AboutComponent } from './public/about/about.component';
-import { LoginComponent } from './admin/auth/login/login.component';
-import { RegisterComponent } from './admin/auth/register/register.component';
-import { DashboardComponent } from './admin/dashboard/dashboard/dashboard.component';
-import { SidenavComponent } from './admin/layouts/sidenav/sidenav.component';
 import { AdminModule } from './admin/admin.module';
 import { HttpClientModule } from '@angular/common/http';
+import { PublicModule } from './public/public.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent,
-    NavbarComponent,
     AnnouncementsComponent,
     PostsComponent,
     RelatedComponent,
@@ -30,6 +23,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     AdminModule,
+    PublicModule,
     HttpClientModule,
   ],
   providers: [],
